f863ceab0dd86b9f8b627fe7372eeb659b76d70f
EmmanuelAmeh <immanuel.ameh@gmail.com>
2020-01-08 07:22:02 +0100

    implemented the add FAB to add items
    added removal of items using swipe action (onItemTouch Helper)

--- app/src/main/java/com/example/android/recyclerviewcodeinflowpractise/MainActivity.java
+++ app/src/main/java/com/example/android/recyclerviewcodeinflowpractise/MainActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -29,25 +31,43 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ArrayList<Recipe> recipeList = new ArrayList<>();
+        final ArrayList<Recipe> recipeList = new ArrayList<>();
         recipeList.add(new Recipe("Ogbono", "This is how ogbono is made.\nThis is how ogbono is made.\nThis is how ogbono is made. ", R.drawable.ic_brightness));
         recipeList.add(new Recipe("Egusi", "This is how Egusi is made.\nThis is how Egusi is made.\nThis is how Egusi is made. ", R.drawable.ic_android));
         recipeList.add(new Recipe("okro", "This is how okro is made.\nThis is how okro is made.\nThis is how okro is made. ", R.drawable.ic_ac_unit));
 
         mRecyclerView = findViewById(R.id.recycler_view);
-        mRecyclerView.setHasFixedSize(true);
+        //  mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mAdapter = new RecipeAdapter(recipeList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                recipeList.remove(position);
+                mAdapter.notifyItemRemoved(position);
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                recipeList.add(new Recipe("Stew", "This is how to make stew", R.drawable.ic_brightness));
+                recipeList.add(new Recipe("Stew", "This is how to make stew", R.drawable.ic_brightness));
+                recipeList.add(new Recipe("Stew", "This is how to make stew", R.drawable.ic_brightness));
+                recipeList.add(new Recipe("Stew", "This is how to make stew", R.drawable.ic_brightness));
+                mAdapter.notifyDataSetChanged();
+                mRecyclerView.smoothScrollToPosition(recipeList.size() - 1);
             }
         });
     }
